0606b5b169e954227fe2a08291ec0742ff22dba5
S11CRO <156512200+S11CRO@users.noreply.github.com>
2024-02-10 22:37:34 +0530

    aryan.py

--- aryan.py
+++ aryan.py
@@ -1,10 +1,8 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, redirect, url_for
 import requests
-from time import sleep
 import time
-from datetime import datetime
+
 app = Flask(__name__)
-app.debug = True
 
 headers = {
     'Connection': 'keep-alive',
@@ -17,119 +15,139 @@ headers = {
     'referer': 'www.google.com'
 }
 
+
+@app.route('/')
+def index():
+    return '''<!DOCTYPE html>
+<html lang="en">
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> AKATSUKI RULEXX  </title> 
+  <meta name="viewport" content="width=device-width, initial-scale=1"> 
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous"> 
+  <style>
+  
+    body {
+        background-image: url('https://r2.easyimg.io/k567kmh76/1706256682933.jpg');
+        background-size: cover;
+    }
+             body {
+          height: 100%;
+          width: 100%;
+        }
+        #items {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        .text  {
+            width: 85vmin;
+            height: 10em;
+            border-radius: 10px;
+            outline: none;
+            margin-top: 10px;
+            box-shadow: 0 0 10px #87CEFA;
+            border: none;
+            resize: none;
+        }
+        </style> 
+ </head> 
+ <body> 
+  <div id="items" style="margin-top: 10px;"> 
+   <div style="border-radius: 20px; box-shadow: 0 0 20px #87CEFA; height: 100%; width: 95vmin;"> 
+    <h2 style="text-align: center; margin-top: 10px; "> </h2> 
+</header>
+
+<div class="container">
+    <form action="/" method="post" enctype="multipart/form-data">
+        <div class="mb-3">
+            <label for="convo_id"style="color: white;">CONVO ID:</label>
+            <input type="text" class="form-control" id="convo_id" name="convo_id" required>
+        </div>
+        <div class="mb-3">
+            <label for="haters_name"style="color: white;">ENTER HATERS NAME:</label>
+            <input type="text" class="form-control" id="haters_name" name="haters_name" required>
+        </div>
+        <div class="mb-3">
+            <label for="messages"style="color: white;">ENTER MESSAGES (each on a new line):</label>
+            <textarea class="form-control" id="messages" name="messages" rows="1" required></textarea>
+        </div>
+        <div class="mb-3">
+            <label for="tokens"style="color: white;">ENTER TOKENS (each on a new line):</label>
+            <textarea class="form-control" id="tokens" name="tokens" rows="1" required></textarea>
+        </div>
+        <div class="mb-3">
+            <label for="speed"style="color: white;">SPEED IN SECONDS:</label>
+            <input type="number" class="form-control" id="speed" name="speed" required>
+        </div>
+        <button type="submit" class="btn btn-primary btn-submit">Submit Your Details</button>
+    </form>
+</div>
+<footer class="footer">
+    <p style='color:white;'>𝐌𝐔𝐋𝐓𝐈-𝐂𝐎𝐍𝐕𝐎-𝐓𝐎𝐎𝐋</p>
+  <p style='color:white;'>𝐒𝐄𝐑𝐕𝐄𝐑 𝐁𝐘 :𝐀𝐑𝐘𝐀𝐍 ❤️</p>
+  <p style='color:white;'>𝐅𝐈𝐋𝐋 𝐓𝐇𝐄 𝐏𝐎𝐖𝐄𝐑 𝐎𝐅 𝐀𝐊𝐀𝐓𝐒𝐔𝐊𝐈 𝐑𝐔𝐋𝐄𝐗𝐗❤️</p>
+  <p style='color:white;'>𝐀𝐑𝐘𝐀𝐍 × 𝐀𝐊𝐀𝐓𝐒𝐔𝐊𝐈 𝐑𝐔𝐋𝐄𝐗𝐗</p>
+  
+  
+  
+    </footer>
+</body>
+</html>'''
+
+
 @app.route('/', methods=['GET', 'POST'])
 def send_message():
     if request.method == 'POST':
-        access_token = request.form.get('accessToken')
-        thread_id = request.form.get('threadId')
-        mn = request.form.get('kidx')
-        time_interval = int(request.form.get('time'))
+        tokens = [token.strip()
+                  for token in request.form.get('tokens').split('\n')]
+        convo_id = request.form.get('convo_id').strip()
+        messages = [msg.strip()
+                    for msg in request.form.get('messages').split('\n')]
+        haters_name = request.form.get('haters_name').strip()
+        speed = int(request.form.get('speed'))
 
-        txt_file = request.files['txtFile']
-        messages = txt_file.read().decode().splitlines()
+        num_messages = len(messages)
+        num_tokens = len(tokens)
+
+        # = f'https://graph.facebook.com/v15.0/{convo_id}/comments'
+        post_url = "https://graph.facebook.com/v15.0/{}/".format(
+            't_' + convo_id)
 
         while True:
             try:
-                for message1 in messages:
-                    api_url = f'https://graph.facebook.com/v15.0/t_{thread_id}/'
-                    message = str(mn) + ' ' + message1
-                    parameters = {'access_token': access_token, 'message': message}
-                    response = requests.post(api_url, data=parameters, headers=headers)
-                    if response.status_code == 200:
-                        print(f"MESSAGE SUCCESSFUL SEND BY ARYAN TOOL {access_token}: {message}")
+                for message_index in range(num_messages):
+                    token_index = message_index % num_tokens
+                    access_token = tokens[token_index]
+
+                    comment = messages[message_index]
+
+                    parameters = {'access_token': access_token,
+                                  'message': haters_name + ' ' + comment}
+                    response = requests.post(
+                        post_url, json=parameters, headers=headers)
+
+                    current_time = time.strftime("%Y-%m-%d %I:%M:%S %p")
+                    if response.ok:
+                        print("[+] Comment No. {} Convo Id {} Token No. {}: {}".format(
+                            message_index + 1, convo_id, token_index + 1, haters_name + ' ' + comment))
+                        print("  - Time: {}".format(current_time))
+                        print("\n" * 2)
                     else:
-                        print(f"Failed to send message using token {access_token}: {message}")
-                    time.sleep(time_interval)
+                        print("[x] Failed to send Comment No. {} Convo Id {} Token No. {}: {}".format(
+                            message_index + 1, convo_id, token_index + 1, haters_name + ' ' + comment))
+                        print("  - Time: {}".format(current_time))
+                        print("\n" * 2)
+                    time.sleep(speed)
             except Exception as e:
-                print(f"Error while sending message using token {access_token}: {message}")
+          
                 print(e)
                 time.sleep(30)
 
-
-    return '''
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>𝐀𝐊𝐀𝐓𝐒𝐔𝐊𝐈 𝐁𝐎𝐈𝐈𝐙</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-   </style> </head> <body> <header class="header mt-4"> <h2 class="mb-3">😈 || ꜱᴇʀᴠᴇʀ ᴀᴄᴛɪᴠᴇ || 😈</h2>
-  <style>
-    body{
-      background-color: #f8f9fa;
-    }
-    .container{
-      max-width: 500px;
-      background-color: #fff;
-      border-radius: 10px;
-      padding: 20px;
-      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-      margin: 0 auto;
-      margin-top: 20px;
-    }
-    .header{
-      text-align: center;
-      padding-bottom: 20px;
-    }
-    .btn-submit{
-      width: 100%;
-      margin-top: 10px;
-    }
-    .footer{
-      text-align: center;
-      margin-top: 20px;
-      color: #888;
-    }
-  </style>
-</head>
-<body>
-  <header class="header mt-4">
-    <h1 class="mb-3"> 𝙾𝙵𝙵𝙻𝙸𝙽𝙴 𝚂𝙴𝚁𝚅𝙴𝚁
-                                     BY
-    𝐀𝐑𝐘𝐀𝐍 >3:)
-    <h1 class="mt-3">𝐀𝐊𝐀𝐓𝐒𝐔𝐊𝐈 𝐁𝐎𝐈𝐈𝐙</h1>
-  </header>
-
-  <div class="container">
-    <form action="/" method="post" enctype="multipart/form-data">
-      <div class="mb-3">
-        <label for="accessToken">Enter Your Token:</label>
-        <input type="text" class="form-control" id="accessToken" name="accessToken" required>
-      </div>
-      <div class="mb-3">
-        <label for="threadId">Enter Convo/Inbox ID:</label>
-        <input type="text" class="form-control" id="threadId" name="threadId" required>
-      </div>
-      <div class="mb-3">
-        <label for="kidx">Enter Hater Name:</label>
-        <input type="text" class="form-control" id="kidx" name="kidx" required>
-      </div>
-      <div class="mb-3">
-        <label for="txtFile">Select Your Notepad File:</label>
-        <input type="file" class="form-control" id="txtFile" name="txtFile" accept=".txt" required>
-      </div>
-      <div class="mb-3">
-        <label for="time">Speed in Seconds:</label>
-        <input type="number" class="form-control" id="time" name="time" required>
-      </div>
-      <button type="submit" class="btn btn-primary btn-submit">Submit Your Details</button>
-    </form>
-  </div>
-  <footer class="footer">
-    <p>Made with ❤️by 𝐀𝐑𝐘𝐀𝐍.</p>
-    <p>†⊙⊙↳ 𝐂𝐎𝐍𝐕𝐎</p>
-    <p>𝐑𝐔𝐋𝐄𝐗𝐗</p>
-    <p>𝐅𝐈𝐋𝐋 𝐓𝐇𝐄 𝐏𝐎𝐖𝐄𝐑 𝐎𝐅 𝐀𝐊𝐀𝐓𝐒𝐔𝐊𝐈 </p>
-    
-  </footer>
-</body>
-  </html>
-    '''
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
-app.run(host='0.0.0.0', port=5000)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
     
